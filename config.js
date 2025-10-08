57bc5ba8d11a571f0fead558cdb5ce032f2568cb
DominicDev <s0l0estudi4r@gmail.com>
2025-10-08 18:28:25 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'IVAN';
+export let nameSongs = [
+	"FOTONOVELA", /*1*/
+	"TE QUIERO TANTO", /*2*/
+	"BAILA", /*3*/
+	"SIN AMOR", /*4*/
+	"HEY MADEMOISELLE ", /*5*/
+	"STARMAN", /*6*/
+	"LUNA", /*7*/
+	"PON LA RADIO", /*8*/
+	"YA NO PUEDO DETENERME", /*9*/
+	"SUEÑOS DE PAPEL", /*10*/
+	"CHICA MISTERIOSA", /*11*/
+	"UN DIA MAS", /*12*/
+	"TAL PARA CUAL", /*13*/
+	"HEROE SUPLENTE", /*14*/
+	"OPERA DE AMOR", /*15*/
+	"PICASSO", /*16*/
+	"NOCHES DE METAL", /*17*/
+	"MUJER DE HIELO", /*18*/
+	"LA SOLEDAD DE UN VIVIDOR DE FONDO", /*19*/
+	"ZENIT", /*20*/
+]
